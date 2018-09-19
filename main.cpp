9ec739b9dcb8de529d71ffb27af6d080c49910e5
Dracunos <markalexander72@gmail.com>
2018-09-19 13:00:45 -0700

    Adding some notes and structure to main()

--- main.cpp
+++ main.cpp
@@ -2,7 +2,23 @@
 #include "output.h"
 
 int main() {
-    std::cout << "Test";
-    testmodule();
+    
+    // create array of gamestates?
+    // std::string gameStates[3] = {"start", "run", "end"};
+    // Maybe we can have the systems handle all the gamestate type logic as well..
+    
+    // create an array of systems
+    // might use function pointers as the values here instead of strings
+    std::string systems[2] = {"display", "input"};
+    
+    // get list of all entities in the game
+    entity entities[]
+    
+    // at least during gameplay gamestate, run entities through each system.
+    // one system should manage the input, output, logic, etc.. this should
+    // keep looping until the program exits.. Let's see how simple we can
+    // make this part of the logic, try to keep as much in the systems as
+    // possible
+    
     return 0;
 }
